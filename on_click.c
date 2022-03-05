ef96e963efd1b7f5fca55e5a49110184044dcdbe
kmilchev <kmilchev@student.42wolfsburg.de>
2022-03-05 16:30:00 +0100

    does not exit if there are collectibles. issue - on game over does not go on top of exit

--- on_click.c
+++ on_click.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 12:41:16 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/03/05 15:11:53 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/03/05 16:29:08 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,51 @@ void on_destroy(mlx_shit *mlx_s)
 	exit(1);
 }
 
-// void on_
+void game_over(mlx_shit *mlx_s)
+{
+	char *string = "Game over";
+	render_map(mlx_s);
+	free_all(mlx_s->map);
+	mlx_string_put(mlx_s->mlx, mlx_s->window, 20, 20, 0x44FF0022, string);
+	
+}
 
 void change_map(mlx_shit *mlx_s, int row, int col)
 {
-	// int cur_row;
-	// int cur_col;
-	// check_borders();
-	mlx_s->coordinates = player_position(*mlx_s);
-	printf("Row: %d, columns %d\n", mlx_s->coordinates.row, mlx_s->coordinates.column);
+	char new_square;
+	static char last_square;
+	static bool was_E = false;
 	
+	printf("Last %c\n", last_square);
+	if (last_square == 'E')
+	{
+		was_E = true;
+	}
+	mlx_s->coordinates = player_position(*mlx_s);
 	row += mlx_s->coordinates.row;
 	col += mlx_s->coordinates.column;
-	mlx_s->map[row][col] = 'P';
-	mlx_s->map[mlx_s->coordinates.row][mlx_s->coordinates.column] = '0';
+	new_square = mlx_s->map[row][col];
+	if (new_square == '1')
+		return ;
 	
-	mlx_s->coordinates = player_position(*mlx_s);
-	printf("Row: %d, columns %d\n", mlx_s->coordinates.row, mlx_s->coordinates.column);
+	if (new_square == 'E' && !(collectibles(mlx_s->map)))
+	{
+		game_over(mlx_s);
+		return ;
+	}
+	last_square = mlx_s->map[row][col];
+	// printf("Last %c\n", last_square);
+	mlx_s->map[row][col] = 'P';
+	if (was_E == false)
+		mlx_s->map[mlx_s->coordinates.row][mlx_s->coordinates.column] = '0';
+	else
+	{
+		mlx_s->map[mlx_s->coordinates.row][mlx_s->coordinates.column] = 'E';
+		was_E = false;
+	}
 	render_map(mlx_s);
+
 }
+
+
+//printf("Row: %d, column %d\n", mlx_s->coordinates.row, mlx_s->coordinates.column);
