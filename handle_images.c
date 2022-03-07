fe68fd0d1c9c2fa2df212c8a9bcb228daf678917
kmilchev <kmilchev@student.42wolfsburg.de>
2022-03-07 11:18:02 +0100

    done. Norminette prrof

--- handle_images.c
+++ handle_images.c
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 11:38:52 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/03/06 16:59:34 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:17:42 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,30 @@
 
 void	render_map(mlx_shit *mlx_s)
 {
-	int		j;
-	int		i;
 	void	*img;
 
-	i = 0;
-	while (mlx_s->map[i] != NULL)
+	mlx_s->row = 0;
+	while (mlx_s->map[mlx_s->row] != NULL)
 	{
-		j = 0;
-		while (mlx_s->map[i][j])
+		mlx_s->col = 0;
+		while (mlx_s->map[mlx_s->row][mlx_s->col])
 		{
-			if (mlx_s->map[i][j] == '1')
-			{
-				img = mlx_s->images.img1;
-			}
-			else if (mlx_s->map[i][j] == '0')
-				img = mlx_s->images.img2;
-			else if (mlx_s->map[i][j] == 'C')
-				img = mlx_s->images.img3;
-			else if (mlx_s->map[i][j] == 'E')
-				img = mlx_s->images.img4;
-			else if (mlx_s->map[i][j] == 'P')
-				img = mlx_s->images.img5;
+			if (mlx_s->map[mlx_s->row][mlx_s->col] == '1')
+				img = mlx_s->images.wall;
+			else if (mlx_s->map[mlx_s->row][mlx_s->col] == '0')
+				img = mlx_s->images.floor;
+			else if (mlx_s->map[mlx_s->row][mlx_s->col] == 'C')
+				img = mlx_s->images.coll;
+			else if (mlx_s->map[mlx_s->row][mlx_s->col] == 'E')
+				img = mlx_s->images.door;
+			else if (mlx_s->map[mlx_s->row][mlx_s->col] == 'P')
+				img = mlx_s->images.player;
 			mlx_put_image_to_window(mlx_s->mlx, mlx_s->window, img,
-				mlx_s->images.img_width * j, mlx_s->images.img_height * i);
-			
-			j++;
+				mlx_s->images.img_width * mlx_s->col,
+				mlx_s->images.img_height * mlx_s->row);
+			mlx_s->col++;
 		}
-		i++;
+		mlx_s->row++;
 	}
 }
 
@@ -49,16 +45,20 @@ t_pics	load_images(mlx_shit *mlx_s)
 {
 	t_pics	images;
 
-	images.img1 = mlx_xpm_file_to_image(mlx_s->mlx,
-			"images/size_w40_h40/wall.xpm", &images.img_width, &images.img_height);
-	images.img2 = mlx_xpm_file_to_image(mlx_s->mlx,
-			"images/size_w40_h40/floor.xpm", &images.img_width, &images.img_height);
-	images.img3 = mlx_xpm_file_to_image(mlx_s->mlx,
-			"images/size_w40_h40/collectible.xpm", &images.img_width, &images.img_height);
-	images.img4 = mlx_xpm_file_to_image(mlx_s->mlx,
-			"images/size_w40_h40/door.xpm", &images.img_width, &images.img_height);
-	images.img5 = mlx_xpm_file_to_image(mlx_s->mlx,
-			"images/size_w40_h40/player.xpm", &images.img_width, &images.img_height);
-	// mlx_destroy_image(mlx_s->mlx, img);
+	images.wall = mlx_xpm_file_to_image(mlx_s->mlx,
+			"images/size_w40_h40/wall.xpm",
+			&images.img_width, &images.img_height);
+	images.floor = mlx_xpm_file_to_image(mlx_s->mlx,
+			"images/size_w40_h40/floor.xpm",
+			&images.img_width, &images.img_height);
+	images.coll = mlx_xpm_file_to_image(mlx_s->mlx,
+			"images/size_w40_h40/collectible.xpm",
+			&images.img_width, &images.img_height);
+	images.door = mlx_xpm_file_to_image(mlx_s->mlx,
+			"images/size_w40_h40/door.xpm",
+			&images.img_width, &images.img_height);
+	images.player = mlx_xpm_file_to_image(mlx_s->mlx,
+			"images/size_w40_h40/player.xpm",
+			&images.img_width, &images.img_height);
 	return (images);
 }
